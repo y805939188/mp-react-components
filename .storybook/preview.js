7d0e9918c612a1fe21c48c38f2d05f02244d2243
DingShinn <y805939188@.163.com>
2023-06-01 21:03:14 +0800

    更新配置

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import '../node_modules/bulma/css/bulma.min.css';
 import '../src/styles.less';
-import '../src/assets/fonts.css';
-import '../src//stories/stories.css';
+// import '../src/assets/fonts.css';
+// import '../src//stories/stories.css';
 
 export const parameters = {
   controls: { expanded: true, sort: 'alpha' },
